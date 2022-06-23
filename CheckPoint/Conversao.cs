ff490bb7dee5e1e0ee47e53d09967a49d7c5cac5
Ariel Lopes <ariellopes888@gmail.com>
2022-06-23 16:40:20 -0300

    Criação dos métodos "Moeda" e "ConverterReal"

--- CheckPoint/Conversao.cs
+++ CheckPoint/Conversao.cs
@@ -6,7 +6,58 @@ using System.Threading.Tasks;
 
 namespace CheckPoint
 {
-    internal class Conversao
+    class Conversao
     {
+        private decimal valorFinal;
+        private Dictionary<string, decimal> moeda = new Dictionary<string, decimal>();
+        public void Moeda()
+        {
+            moeda.Add("dolar", 4.50M);
+            moeda.Add("euro", 6.20M);
+            moeda.Add("iene", 0.052M);
+            moeda.Add("libra", 6.95M);
+        }
+
+        public decimal ConverterReal(int opcao, decimal valorRecebido)
+        {
+            switch (opcao)
+            {
+                case 1:
+                    valorFinal = valorRecebido / moeda["dolar"];
+                    break;
+                case 2:
+                    valorFinal = valorRecebido / moeda["euro"];
+                    break;
+                case 3:
+                    valorFinal = valorRecebido / moeda["iene"];
+                    break;
+                case 4:
+                    valorFinal = valorRecebido / moeda["libra"];
+                    break;
+            }
+            return valorFinal;
+        }
+
+        public decimal ConverterParaReal(int opcao, decimal valorRecebido)
+        {
+            switch (opcao)
+            {
+                case 1:
+                    valorFinal = valorRecebido * moeda["dolar"];
+                    break;
+                case 2:
+                    valorFinal = valorRecebido * moeda["euro"];
+                    break;
+                case 3:
+                    valorFinal = valorRecebido * moeda["iene"];
+                    break;
+                case 4:
+                    valorFinal = valorRecebido * moeda["libra"];
+                    break;
+            }
+            return valorFinal;
+        }
+
     }
 }
+
